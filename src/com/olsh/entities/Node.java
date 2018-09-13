9fa0d3ed515d5fd2fdbc27d31f45a3b53e2ab06d
Olha Schekera <kaltregen@gmail.com>
2018-09-13 23:15:12 +0300

    Create Node.java

---
+++ src/com/olsh/entities/Node.java
@@ -0,0 +1,11 @@
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
