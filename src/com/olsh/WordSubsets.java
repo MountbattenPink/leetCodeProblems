cc50b70bd96fc4a22188b3d5a6d515956bd20e6a
Olha Schekera <kaltregen@gmail.com>
2018-10-09 00:22:02 +0300

    Update WordSubsets.java

--- src/com/olsh/WordSubsets.java
+++ src/com/olsh/WordSubsets.java
@@ -1,4 +1,6 @@
 /**
+#916. Word Subsets
+
 We are given two arrays A and B of words.  Each word is a string of lowercase letters.
 
 Now, say that word b is a subset of word a if every letter in b occurs in a, including multiplicity.  For example, "wrr" is a subset of "warrior", but is not a subset of "world".
