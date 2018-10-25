09184f174e83a66881d835d95ce0f6507d57fd1a
Olha Schekera <kaltregen@gmail.com>
2018-10-25 13:40:39 +0300

    Create FizzBuzz.java

---
+++ src/com/olsh/FizzBuzz.java
@@ -0,0 +1,24 @@
+/**
+#412. Fizz Buzz
+Write a program that outputs the string representation of numbers from 1 to n.
+But for multiples of three it should output “Fizz” instead of the number and for 
+the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+Example:
+n = 15,
+Return:
+["1", "2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+*/
+package com.olsh;
+class FizzBuzz {
+ public List < String > fizzBuzz(int n) {
+  List < String > res = new ArrayList < String > ();
+  for (int i = 1; i <= n; i++) {
+   if (i % 3 == 0) {
+    if (i % 5 == 0) res.add("FizzBuzz");
+    else res.add("Fizz");
+   } else if (i % 5 == 0) res.add("Buzz");
+   else res.add(String.valueOf(i));
+  }
+  return res;
+ }
+}
