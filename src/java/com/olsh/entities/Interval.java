4012dc88f7638e3916b06139fc5ec9924618b72a
Olha Schekera <kaltregen@gmail.com>
2020-02-04 17:51:41 +0200

    Create Interval.java

---
+++ src/java/com/olsh/entities/Interval.java
@@ -0,0 +1,11 @@
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+}
